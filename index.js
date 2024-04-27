fc0cc42ebbb260fb0d13451835301d85cba74d80
pavan naik <pavanpnaik321@gmail.com>
2024-04-27 11:40:13 +0530

    Simple status

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get('/',(req,res)=>{
     res.send('server is running')
 })
 
+app.get('/test',(req,res)=>{
+    res.send('test the deployment')
+})
 app.listen(8000,()=>{
     console.log('server is running on port 8000')
 })
