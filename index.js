d432d6ea527619f4f6b0e31ae0f44b8616238ef9
pavan naik <pavanpnaik321@gmail.com>
2024-04-27 13:54:02 +0530

    backend

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const PORT = 8000 || 3000
-// middlewares
+const PORT = 8000 || 3000;
+
+// Middlewares
 app.use(cors({
-    credentials: true,
-    origin: ["http://localhost:5173","http://localhost:5173"],
-    methods:["POST","GET","PUT"],
-  },{
-    credentials: true,
-    origin: ["http://localhost:5173","http://localhost:5174"],
-    methods:["POST","GET","PUT"],
-  }));
+  credentials: true,
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  methods: ["POST", "GET", "PUT"],
+}));
+
 app.use(express.json());
 
-
-const connection = require('./db/Configuration')
+const connection = require('./db/Configuration');
 connection();
 
+app.get('/', (req, res) => {
+  res.send('server is running');
+});
 
-app.get('/',(req,res)=>{
-    res.send('server is running')
-})
+app.get('/test', (req, res) => {
+  res.send('test the deployment');
+});
 
-app.get('/test',(req,res)=>{
-    res.send('test the deployment')
-})
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
