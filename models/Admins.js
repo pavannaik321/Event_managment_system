9bd04b0359fdf0dc714ded28f53119e5409ce399
AntoTomAbraham <antotom53@gmail.com>
2024-04-27 15:11:02 +0530

    adminmodel

--- models/Admins.js
+++ models/Admins.js
@@ -1,1 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true 
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true 
+  },
+  password: {
+    type: String,
+    required: true
+  },
+}, { timestamps: true }); 
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = Admin;
+
+
